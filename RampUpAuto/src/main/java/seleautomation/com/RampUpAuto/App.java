9a5a93683c83d4e3eb65b9db23675e0ccb9135a0
inasnour96 <32929021+inasnour96@users.noreply.github.com>
2023-12-10 23:25:17 +0200

    Getting starting with selenium

--- RampUpAuto/src/main/java/seleautomation/com/RampUpAuto/App.java
+++ RampUpAuto/src/main/java/seleautomation/com/RampUpAuto/App.java
@@ -1,13 +1,29 @@
 package seleautomation.com.RampUpAuto;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class App {
+    public static void main(String[] args) {
+        WebDriver driver = null;
+        WebDriverManager.chromedriver().browserVersion("120.0.6099.71").setup();
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("Selenium Tutorials");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+        if (driver.findElement(By.name("q")).getText().equals("Selenium Tutorials")) {
+            System.out.println("Text Assertion Passed");
+        } else {
+            System.out.println("Text Assertion Failed");
+        }
+
+        driver.close();
     }
 }

3b6f387a463a43eb493c9a6be70ddd29b204f5ec
inasnour96 <32929021+inasnour96@users.noreply.github.com>
2023-12-16 23:02:51 +0200

    pull request changes

--- RampUpAuto/src/main/java/seleautomation/com/RampUpAuto/App.java
+++ RampUpAuto/src/main/java/seleautomation/com/RampUpAuto/App.java
@@ -9,19 +9,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class App {
     public static void main(String[] args) {
 
-        
         WebDriver driver = null;
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\enoor\\Desktop\\Authomation\\EnasSelenium\\ChromeDriver.exe");
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\enoor\\Desktop\\Authomation\\EnasSelenium\\ChromeDriver.exe");
         ChromeOptions options = new ChromeOptions();
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com/");
         driver.findElement(By.name("q")).sendKeys("Selenium Tutorials");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 
-        String expectedTitle = "Selenium Tutorisals - Google Search";
-        String actualTitle = driver.findElement(By.name("q")).getText();
+        String expectedTitle = "Selenium Tutorials - Google Search";
+        String actualTitle = driver.getTitle();
         assert driver.getTitle().equals(expectedTitle)
-                 : "Text Assertion Failed! Expected title: " + expectedTitle + ", Actual title: " + actualTitle;
+                : "Text Assertion Failed! Expected title: " + expectedTitle + ", Actual title: " + actualTitle;
         driver.close();
     }
 }

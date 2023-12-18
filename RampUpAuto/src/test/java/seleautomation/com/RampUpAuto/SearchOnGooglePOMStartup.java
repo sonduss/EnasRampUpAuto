405cccb62a957f0d8f47f598c98c20181f67432f
inasnour96 <32929021+inasnour96@users.noreply.github.com>
2023-12-18 22:57:26 +0200

    add annotations for Search on GooglePOM

--- RampUpAuto/src/test/java/seleautomation/com/RampUpAuto/SearchOnGooglePOMStartup.java
+++ RampUpAuto/src/test/java/seleautomation/com/RampUpAuto/SearchOnGooglePOMStartup.java
@@ -4,25 +4,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SearchOnGooglePOMStartup {
+    SearchOnGooglePOM googlePage;
+    WebDriver driver = null;
 
-    @Test
-    public void searchOnGoogle() {
-        WebDriver driver = null;
+    @BeforeClass
+    public void beforeClass() {
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\enoor\\Desktop\\Authomation\\EnasRampUpAuto\\ChromeDriver.exe");
         ChromeOptions options = new ChromeOptions();
         driver = new ChromeDriver(options);
 
-        SearchOnGooglePOM googlePage = new SearchOnGooglePOM(driver);
+        googlePage = new SearchOnGooglePOM(driver);
         googlePage.openGoogle();
+    }
+
+    @Test
+    public void searchOnGoogle() {
+
         googlePage.searchFor("Selenium Tutorials");
 
         String expectedTitle = "Selenium Tutorials - Google Search";
         String actualTitle = googlePage.getTitle();
         Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @AfterClass
+    public void afterClass() {
         driver.close();
     }
+
 }
